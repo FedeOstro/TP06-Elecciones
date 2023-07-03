88da734ecbb96574a517b12841bdbfe90547669d
FEDERICO ILAN OSTROVSKY <47434260@ort>
2023-07-03 09:04:09 -0300

    Cuarta Ver

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,13 +13,13 @@ public class HomeController : Controller
     public IActionResult VerDetallePartido(int IdPartido)
     {
         ViewBag.InfoPartido = BD.VerInfoCandidato(IdPartido);
-        ViewBag.ListaCandidatos = BD.ListaCandidatos(IdPartido);
+        ViewBag.ListaCandidatos = BD.ListarCandidatos(IdPartido);
         return View();
     }
 
     public IActionResult VerDetalleCandidato(int idCandidato)
     {   
-        ViewBag.VerInfoCandidato = BD.VerDetalleCandidato(idCandidato);
+        ViewBag.VerInfoCandidato = BD.VerInfoCandidato(idCandidato);
         return View();
     }
 
@@ -32,13 +32,13 @@ public class HomeController : Controller
     [HttpPost] IActionResult GuardadCandidato(Candidato can)
     {
         BD.AgregarCantidato(can);
-        VerDetallePartido(can.IdPartido);
+        return VerDetallePartido(can.IdPartido);
     }
 
     IActionResult EliminarCandidato(int idCandidato, int idPartido)
     {
         BD.EliminarCandidato(idCandidato);
-        VerDetallePartido(idPartido);
+        return VerDetallePartido(idPartido);
     }
 
     IActionResult Elecciones()
